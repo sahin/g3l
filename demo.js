572f25db87c496da3ce6cb238f670dd9cc9d5739
cagataycali <calicagatay@icloud.com>
2016-09-13 21:03:38 -0700

    Hot bug fix

--- demo.js
+++
@@ -1,8 +0,0 @@
-var pathExists = require('path-exists');
-var async = require('async');
-
-module.exports = function (path) {
-  pathExists('~/.g3l.json')
-    .then(function(exists) {exists ? resolve() : null;})
-    .catch((err) => {reject()});
-}
