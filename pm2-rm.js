2d0a0c176b1a0d4b15bba8b18986f5802030fb68
cagataycali <calicagatay@icloud.com>
2016-09-16 22:32:48 -0700

    🎉  Auto commit try ⚰

--- pm2-rm.js
+++ pm2-rm.js
@@ -1,4 +1,5 @@
 var globalModulesDir = require('global-modules');
 var pm2 = require('pm2');
-pm2.delete('index', function(err) {})
+pm2.delete('0', function(err) {})
 pm2.disconnect();
+process.exit();
